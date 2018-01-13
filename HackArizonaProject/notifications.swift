bac50a206302a8070faeccd409860d33e055725b
ual-laptop <ual-laptop@dhcp-10-134-236-188.uawifi.arizona.edu>
2018-01-13 10:43:11 -0700

    Added notifications

--- HackArizonaProject/notifications.swift
+++ HackArizonaProject/notifications.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import UserNotifications
 
 class notifications: UIViewController {
-
+    
+    static var theNotifications = [UNMutableNotificationContent]()
+    var dayCode = ["Sunday":1, "Monday":2, "Tuesday":3, "Wednesday":4, "Thursday":5, "Friday":6, "Saturday":7]
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "You chose Spiderman!"
+        content.subtitle = "Green Goblin"
+        content.body = "Let die the woman you love or suffer the little children!"
+        content.badge = 1
+        var date = DateComponents()
+        date.hour = 10
+        date.minute = 7
+        date.weekday = 7
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+        let request = UNNotificationRequest(identifier: "Times up!", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        notifications.theNotifications.append(content)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {didAllow, error in})
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
