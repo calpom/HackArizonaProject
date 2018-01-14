47edb04c949211b3ee4438abda8931ce040cfd3c
coderoder <codyjwalker@email.arizona.edu>
2018-01-13 18:50:29 -0700

    almost dun wit da buttons

--- HackArizonaProject/WeekdaySelector.swift
+++ HackArizonaProject/WeekdaySelector.swift
@@ -57,10 +57,58 @@ import UIKit
         }
         dayButtons.removeAll()
         
-        for _ in 0..<dayButtonCount {
+        let bundle = Bundle(for: type(of: self))
+        
+        let mondayButton = UIImage(named: "Monday", in: bundle, compatibleWith: self.traitCollection)
+        let tuesdayButton = UIImage(named: "Tues-Thur", in: bundle, compatibleWith: self.traitCollection)
+        let wednesdayButton = UIImage(named: "Wednesday", in: bundle, compatibleWith: self.traitCollection)
+        let thursdayButton = UIImage(named: "Tues-Thur", in: bundle, compatibleWith: self.traitCollection)
+        let fridayButton = UIImage(named: "Friday", in: bundle, compatibleWith: self.traitCollection)
+        let saturdayButton = UIImage(named: "Sun-Sat", in: bundle, compatibleWith: self.traitCollection)
+        let sundayButton = UIImage(named: "Sun-Sat", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        let darkMondayButton = UIImage(named: "darkMonday", in: bundle, compatibleWith: self.traitCollection)
+        let darkTuesdayButton = UIImage(named: "darkTuesdayThursday", in: bundle, compatibleWith: self.traitCollection)
+        let darkWednesdayButton = UIImage(named: "darkWednesday", in: bundle, compatibleWith: self.traitCollection)
+        let darkThursdayButton = UIImage(named: "darkTuesdayThursday", in: bundle, compatibleWith: self.traitCollection)
+        let darkFridayButton = UIImage(named: "darkFriday", in: bundle, compatibleWith: self.traitCollection)
+        let darkSaturdayButton = UIImage(named: "darkSundaySaturday", in: bundle, compatibleWith: self.traitCollection)
+        let darkSundayButton = UIImage(named: "darkSundaySaturday", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        for index in 0..<dayButtonCount {
             // Create button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+
+            // Set button images
+            switch index
+            {
+            case 0:
+                button.setImage(mondayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 1:
+                button.setImage(tuesdayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 2:
+                button.setImage(wednesdayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 3:
+                button.setImage(thursdayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 4:
+                button.setImage(fridayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 5:
+                button.setImage(saturdayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            case 6:
+                button.setImage(sundayButton, for: .normal)
+                button.setImage(darkMondayButton, for: .selected)
+            default:
+                break
+                
+            }
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
